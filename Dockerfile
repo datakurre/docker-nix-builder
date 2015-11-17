dac66756559edd1ff8c0000d0cb9ab6ef0b4ffba
Asko Soukka <asko.soukka@iki.fi>
2015-11-18 01:08:30 +0200

    Simplify

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM datakurre/nix
 MAINTAINER asko.soukka@iki.fi
-RUN nix-env -f '<nixpkgs>' -iA gnutar -iA graphviz
+RUN nix-env -f '<nixpkgs>' -iA gnutar
 COPY build.sh /home/user/
 ENTRYPOINT ["/home/user/build.sh"]
--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 mkdir tmp
-nix-build $1
+nix-build $1 > /dev/null
 if [ -h result/etc ]; then echo Error: Build resulted /etc as symlink && exit 1; fi
-nix-store -q result --graph | sed 's/#ff0000/#ffffff/' | dot -Nstyle=bold -Tpng > $1.png
-tar cvz --transform="s|^result/||" tmp `nix-store -qR result` result/* > $1.tar.gz
+tar cvz --transform="s|^result/||" tmp `nix-store -qR result` result/*
